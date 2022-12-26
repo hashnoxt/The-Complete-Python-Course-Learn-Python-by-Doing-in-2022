cb4482f071e84247beaad87b1c8675e4607ccfea
Hashan Nalaka <hashannalaka0123@gmail.com>
2022-12-26 18:32:17 +0530

    multiline strings

--- 01/les3.py
+++ 01/les3.py
@@ -4,3 +4,12 @@ my_string = "Hello, world"
 
 print(my_string)
 
+string_with_quotes = "Hello 'This is string inside quotes' ===> "
+
+multiline_string = """ This is 
+multiline 
+String
+"""
+print(multiline_string)
+
+
